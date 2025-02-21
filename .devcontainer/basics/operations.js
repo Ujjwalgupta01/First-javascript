a9f95658f82ff1ed35f3147db42333efc4beefb6
Ujjwal gupta <ankitgupta1441@gmail.com>
2025-02-21 12:00:44 +0000

    operation in js

---
+++ .devcontainer/basics/operations.js
@@ -0,0 +1,21 @@
+let a=5
+let b=10
+
+console.log(a+b)
+console.log(a-b)
+console.log(b-a)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+let str1="Ujjwal"
+let str2="Gupta"
+let str3=str1+str2
+
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(2+2+"1")
