504c222c7dc1381319a813194d1ebcfad9c4cb1b
Ujjwal gupta <ankitgupta1441@gmail.com>
2025-02-21 11:41:14 +0000

    conversion of datatype

---
+++ .devcontainer/basics/datatype_conversion.js
@@ -0,0 +1,16 @@
+let score=33
+console.log(score)
+console.log(typeof score)
+
+console.log(typeof (score))
+// this is a method
+
+let age="33"
+console.log(age)
+console.log(typeof age)
+
+// data_type conversion
+let myValue= Number(age)
+console.log(myValue)
+console.log(typeof myValue)
+
