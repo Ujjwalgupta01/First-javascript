256880029dce6896a1b3d5a0b8e5c5d3be71a190
Ujjwal gupta <ankitgupta1441@gmail.com>
2025-02-21 10:58:36 +0000

    second commit

---
+++ .devcontainer/basics/data_type.js
@@ -0,0 +1,34 @@
+"use strict"  
+// treat all vs code as newer version
+
+let name="Ujjwal Gupta"
+// String data type
+
+let age=16
+// number data type
+
+let isLoggedIn=true
+// boolean data type
+
+// alert("hello")
+// // Not worked in nodejs work only in browser to give alert
+
+console.log(1+2)
+
+let state; 
+// undefined
+
+let city=null
+// null is standalone value
+
+console.log(typeof name)
+
+console.log(typeof "Ujjwal Gupta")
+
+console.log(typeof (age))
+
+console.log(typeof age)
+
+console.log(typeof "16")
+
+console.log(typeof (16))
---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,17 @@
+const accountId="123456"
+let accountName="Ujjwal Gupta"
+var accountPassword="232101"
+accountCity="Varanasi"
+
+// accountId="654321"
+accountName="Preeti Gupta"
+accountPassword="1441"
+ let accountState;
+
+ console.log(accountId)
+ console.log(accountName)
+ console.log(accountPassword)
+ console.log(accountCity)
+ console.log(accountState)
+
+ console.table([accountId,accountName,accountPassword,accountState,accountCity])
